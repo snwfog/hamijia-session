8e625b9fd410e52653f257a7d1658f2f9d8171bf
Charles Yang <donchoa@gmail.com>
2015-06-10 22:07:48 -0400

    Change db connection

--- db_access.rb
+++ db_access.rb
@@ -3,7 +3,9 @@ include RethinkDB::Shortcuts
 
 module Helpers
   module DbAccess
-    db_config = YAML.load_file('database.yml')
-    CONN      = r.connect(db_config).use("#{db_config['database']}_#{db_config['suffix']}")
+    db_config                            = YAML.load_file('database.yml')
+    port, host, database, suffix, tables = db_config['port'], db_config['host'], db_config['database'], db_config['suffix'], db_config['tables']
+    COMPLETE_DB_NAME                     = database + '_' + suffix
+    CONN                                 = r.connect(host: host, port: port, db: COMPLETE_DB_NAME)
   end
 end
--- hamijia_ls_session.rb
+++ hamijia_ls_session.rb
@@ -3,11 +3,15 @@ require 'eldr'
 require_relative 'db_access'
 
 class HamijiaLsSession < Eldr::App
+  include RethinkDB::Shortcuts
   get '/', -> { [200, { 'Content-Type' => 'txt' }, ['O hai der; serving hamijia-ls-session']] }
 
   post '/lssession' do |env|
     conn = Helpers::DbAccess::CONN
-    puts conn
-    puts env
+    request = Rack::Request.new(env)
+    hash = JSON.parse(request.body.read.to_s)
+    resp = r.table('ha_ls_sessions').insert(hash).run(conn)
+
+    Rack::Response.new(resp.to_json)
   end
 end
