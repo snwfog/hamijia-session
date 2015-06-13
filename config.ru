874368ba4839af981a5e1caffc9026a589e85e94
Charles Yang <donchoa@gmail.com>
2015-06-13 12:48:50 -0400

    Change the req resp ls session

--- config.ru
+++ config.ru
@@ -2,6 +2,7 @@ require_relative 'hamijia_api_log'
 require_relative 'hamijia_api_authentication'
 require_relative 'hamijia_ls_session'
 
+use Rack::Session::Cookie
 use HamijiaApiLog
 use HamijiaApiAuthentication
 run HamijiaLsSession
--- hamijia_ls_session.rb
+++ hamijia_ls_session.rb
@@ -1,15 +1,17 @@
 require 'eldr'
+require 'digest'
 
 require_relative 'db_access'
 
 class HamijiaLsSession < Eldr::App
+  include RethinkDB::Shortcuts
+
   HA_LS_SESSION_TABLE = 'ha_ls_sessions'
 
   before do
     @conn = Helpers::DbAccess::CONN
   end
 
-  include RethinkDB::Shortcuts
   get '/', -> { [200, { 'Content-Type' => 'txt' }, ['O hai der; serving hamijia-ls-session']] }
 
   get '/lssession/:ls_session_id' do |env|
@@ -19,18 +21,10 @@ class HamijiaLsSession < Eldr::App
     # Try to locate this session_id in the database
     db_resp       = r.table(HA_LS_SESSION_TABLE).get(ls_session_id).run(@conn)
 
-    response = Rack::Response.new
-
-    if db_resp
-      response.body = [{ elements: [db_resp] }.to_json]
-    else
-      response.body = [{ elements: [] }.to_json]
-    end
-
-    response.header['HTTP_X_API_LOG_ID'] = req.env['HTTP_X_API_LOG_ID']
-    response.header['Content-Type']      = 'application/json'
-
-    response
+    Rack::Response.new([{ elements: [db_resp] }.to_json], 200, {
+                                                          'HTTP_X_API_LOG_ID' => hash['api_request_log_id'],
+                                                          'Content-Type'      => 'application/json'
+                                                        })
   end
 
   post '/lssession' do |env|
@@ -40,10 +34,16 @@ class HamijiaLsSession < Eldr::App
     hash['api_request_log_id'] = req.env['HTTP_X_API_LOG_ID']
 
     db_resp = r.table(HA_LS_SESSION_TABLE).insert(hash).run(@conn)
+    resp = Rack::Response.new([db_resp.to_json], 201, {
+                                                 'HTTP_X_API_LOG_ID' => hash['api_request_log_id'],
+                                                 'Content-Type'      => 'application/json'
+                                               })
+    # Set cookie back _halssession
+    unless db_resp['errors'] > 0
+      cookie_halssession = Digest::SHA2.new(256).hexdigest(db_resp['generated_keys'].first)
+      resp.env['rack.session']['_halsession'] = cookie_halssession
+    end
 
-    Rack::Response.new([db_resp.to_json], 201, {
-                                          'HTTP_X_API_LOG_ID' => hash['api_request_log_id'],
-                                          'Content-Type'      => 'application/json'
-                                        })
+    resp
   end
 end
